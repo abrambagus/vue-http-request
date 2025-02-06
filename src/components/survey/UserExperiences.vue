123169b1236221d1ea7deb3a38a37ae91060c746
abram <abrambagus@gmail.com>
2025-02-06 10:18:15 +0700

    add handle data error

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -8,10 +8,11 @@
         </base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">
         No stored experiences found. Start adding survey result first.
       </p>
-      <ul v-else-if="!isLoading && results && results.length > 0">
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -34,11 +35,13 @@ export default {
     return {
       results: [],
       isLoading: false,
+      error: null,
     };
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
       fetch(
         'https://vue-http-demo-b73b6-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
       )
@@ -58,6 +61,10 @@ export default {
             });
           }
           this.results = results;
+        })
+        .catch(() => {
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later.';
         });
     },
   },
